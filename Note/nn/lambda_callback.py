94e4387e299ed72225fb4be3b511cae3485b9414
NoteDance <notedance@outlook.com>
2025-02-03 23:10:11 +0800

    Update lambda_callback.py

--- Note/nn/lambda_callback.py
+++ Note/nn/lambda_callback.py
@@ -4,6 +4,8 @@ class LambdaCallback:
                  on_train_end=None,
                  on_epoch_begin=None,
                  on_epoch_end=None,
+                 on_episode_begin=None,
+                 on_episode_end=None,
                  on_batch_begin=None,
                  on_batch_end=None,
                  on_test_begin=None,
@@ -12,6 +14,8 @@ class LambdaCallback:
         self.on_train_end = on_train_end
         self.on_epoch_begin = on_epoch_begin
         self.on_epoch_end = on_epoch_end
+        self.on_episode_begin = on_episode_begin
+        self.on_episode_end = on_episode_end
         self.on_batch_begin = on_batch_begin
         self.on_batch_end = on_batch_end
         self.on_test_begin = on_test_begin
